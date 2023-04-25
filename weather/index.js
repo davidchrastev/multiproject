04e91c11f2ef9de234fc784620f97581872b4b14
Давид Кръстев <106105745+davidchrastev@users.noreply.github.com>
2023-04-25 12:55:36 +0300

    basic weather front end using openweather api.

---
+++ weather/index.js
@@ -0,0 +1,46 @@
+function attachEvents() {
+    const apiKey = '5db261d52100e489bc5f62a18ed7b485';
+    const apiUrl  = 'https://api.openweathermap.org/data/2.5/weather?units=metric&q=';
+    const temp = document.querySelector('.temp');
+    const humidity = document.querySelector('.humidity');
+    const windSpeed = document.querySelector('.wind');
+    const input = document.querySelector('.search input');
+    const searchBtn = document.querySelector('.search button');
+    const weatherIcon = document.querySelector('.weather-icon');
+
+    searchBtn.addEventListener('click', ()=> {
+        checkWeather(input.value);
+
+    })
+
+
+     async function checkWeather(city) {
+        const response = await fetch(apiUrl + city + `&appid=${apiKey}`);
+        let data = await response.json();
+        document.querySelector('.city').innerHTML = data.name;
+        temp.innerHTML = Math.round(data.main.temp) + '°c';
+        humidity.innerHTML = data.main.humidity + '%';
+        windSpeed.innerHTML = data.wind.speed + ' km/h';
+
+        if (data.weather[0].main === 'Clouds') {
+            weatherIcon.src = 'images/clouds.png';
+        } else if (data.weather[0].main === 'Clear') {
+            weatherIcon.src = 'images/clear.png';
+        } else if (data.weather[0].main === 'Rain') {
+            weatherIcon.src = 'images/rain.png';
+        } else if (data.weather[0].main === 'Drizzle') {
+            weatherIcon.src = 'images/drizzle.png';
+        } else if (data.weather[0].main === 'Mist') {
+            weatherIcon.src = 'images/mist.png';
+        }
+
+
+
+         input.value = '';
+    }
+
+
+}
+
+
+attachEvents();
