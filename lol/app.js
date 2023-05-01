75f9610d584dbb643049f9717643b1af175ccdaa
Давид Кръстев <106105745+davidchrastev@users.noreply.github.com>
2023-05-01 19:56:56 +0300

    League of legends adding more info about the champions

--- lol/app.js
+++ lol/app.js
@@ -1,10 +1,10 @@
 function attachEvents() {
-    const input = document.querySelector('.summonerName')
-    const btn = document.querySelector('.submit');
-    const BASE_URL = 'https://euw1.api.riotgames.com/lol/summoner/v4/summoners/by-name/';
+    // const input = document.querySelector('.summonerName')
+    // const btn = document.querySelector('.submit');
+    // const BASE_URL = 'https://euw1.api.riotgames.com/lol/summoner/v4/summoners/by-name/';
     const CHAMPIONS_ULR = 'https://ddragon.leagueoflegends.com/cdn/6.24.1/data/en_US/champion.json';
-    const API_KEY = '?api_key=RGAPI-39fa2939-cd41-4ae2-b865-68b8b77ac7cc';
-    const CHAMPION_IMAGE = 'http://ddragon.leagueoflegends.com/cdn/img/champion/splash/Ahri_0.jpg';
+    // const API_KEY = '?api_key=RGAPI-39fa2939-cd41-4ae2-b865-68b8b77ac7cc';
+    const CHAMPION_IMAGE = 'http://ddragon.leagueoflegends.com/cdn/img/champion/splash/';
     const champs = document.querySelector('.container');
 
     const fetchSummoner = () => {
@@ -28,11 +28,15 @@ function attachEvents() {
                     let currentChampion = championNames[i];
                     const leagueChamp = {
                         name: currentChampion,
+                        title: data.data[currentChampion].title,
                         blurb: data.data[currentChampion].blurb,
-                        image: 'http://ddragon.leagueoflegends.com/cdn/img/champion/splash/' + currentChampion + '_0.jpg',
+                        image: CHAMPION_IMAGE + currentChampion + '_0.jpg',
+                        attack: data.data[currentChampion].info.attack,
+                        defense: data.data[currentChampion].info.defense,
+                        difficulty: data.data[currentChampion].info.difficulty,
+                        magic: data.data[currentChampion].info.magic,
+                        type: data.data[currentChampion].tags[0] + ' ' + data.data[currentChampion].tags[1],
                     }
-
-                    console.log(leagueChamp);
 
                     console.log(data.data[currentChampion]);
                     displayChamp(leagueChamp);
@@ -45,8 +49,15 @@ function attachEvents() {
         const champsString = `
         <li class="card">
             <img class="card-image" src="${champion.image}"/>
-            <h2 class="card-title">${champion.name}</h2>
+            <h2 class="card-title">${champion.name} "${champion.title}"</h2>           
             <p class="card-subtitle">Type: ${champion.blurb}</p>
+            <p class="card-subtitle">
+                Attack: ${champion.attack},<br>
+                Defence: ${champion.defense},<br>
+                Difficulty: ${champion.difficulty},<br>
+                Magic: ${champion.magic},<br>
+                Type: ${champion.type}
+            </p>
         </li>
     `;
         champs.innerHTML += champsString;
