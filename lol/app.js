1ed2667a891fcbb401a7d719b4b6c19db9bfe2a1
Давид Кръстев <106105745+davidchrastev@users.noreply.github.com>
2023-05-22 16:04:29 +0300

    Using my skills to connect my other project but i am finding difficulty with the cors.

--- lol/app.js
+++ lol/app.js
@@ -1,18 +1,18 @@
 function attachEvents() {
-    // const input = document.querySelector('.summonerName')
-    // const btn = document.querySelector('.submit');
-    // const BASE_URL = 'https://euw1.api.riotgames.com/lol/summoner/v4/summoners/by-name/';
+    const input = document.querySelector('.summonerName')
+    const btn = document.querySelector('.submit');
+    const BASE_URL = 'https://euw1.api.riotgames.com/lol/summoner/v4/summoners/by-name/';
     const CHAMPIONS_ULR = 'https://ddragon.leagueoflegends.com/cdn/6.24.1/data/en_US/champion.json';
-    // const API_KEY = '?api_key=RGAPI-39fa2939-cd41-4ae2-b865-68b8b77ac7cc';
+    const API_KEY = '?api_key=RGAPI-39fa2939-cd41-4ae2-b865-68b8b77ac7cc';
     const CHAMPION_IMAGE = 'http://ddragon.leagueoflegends.com/cdn/img/champion/splash/';
     const champs = document.querySelector('.container');
-    // const fetchSummoner = () => {
-    //     fetch(BASE_URL + input.value + API_KEY)
-    //         .then((res) => res.json())
-    //         .then((data) => {
-    //             console.log(data);
-    //         })
-    // };
+    const fetchSummoner = () => {
+        fetch(BASE_URL + input.value + API_KEY)
+            .then((res) => res.json())
+            .then((data) => {
+                console.log(data);
+            })
+    };
     const championNames = [];
     const fetchChampions = () => {
         fetch(CHAMPIONS_ULR)
---
+++ todo-project/app.js
@@ -0,0 +1,171 @@
+function attachEvents() {
+    const BASE_URL = 'http://localhost:8080/tasks/1';
+
+    const inputs = {
+        loadBoardBtn: document.getElementById('load-board-btn'),
+        title: document.getElementById('title'),
+        description: document.getElementById('description'),
+        createTaskBtn: document.getElementById('create-task-btn'),
+    }
+    const container = {
+        toDo: document.getElementById('todo-section'),
+        done: document.getElementById('done-section'),
+    }
+    inputs.loadBoardBtn.addEventListener('click', load);
+    let currentTasks = [];
+    function load(event) {
+        if (event) {
+            event.preventDefault();
+        }
+
+        container.toDo.children[1].innerHTML = '';
+
+        fetch(BASE_URL)
+            .then((res) => res.json())
+            .then((allProducts) => {
+                currentTasks = Object.values(allProducts);
+                let arr = currentTasks[3];
+                console.log(arr);
+
+                for (const { description, completionStatus } of arr) {
+
+                    switch (completionStatus) {
+                        case false:
+                            const li = createElement('li', container.toDo.children[1],'', ['task']);
+                            createElement('p', li, description);
+                            const moveToProgress = createElement('button', li, 'Move to Done');
+                            moveToProgress.addEventListener('click', toDone);
+                            break;
+                        case true:
+                            const liDone = createElement('li', container.done.children[1],'', ['task']);
+                            createElement('p', liDone, description);
+                            const close = createElement('button', liDone, 'Close');
+                            close.addEventListener('click', deleteTask);
+
+                            break;
+                    }
+                }
+
+            })
+    }
+    function deleteTask() {
+        const id = this.parentNode.id;
+
+        const httpHeaders = {
+            method: 'DELETE'
+        }
+        fetch(`${BASE_URL}${id}`, httpHeaders)
+            .then(() => {
+                load();
+            })
+            .catch((err) => {
+                console.error(err);
+            })
+    }
+
+    function toDone() {
+        const id = this.parentNode.id;
+        console.log(id);
+
+        const payload = JSON.stringify({
+            completionStatus: true,
+        });
+
+        const httpHeaders = {
+            method: 'UPDATE',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: payload,
+            mode: 'cors', // Add the 'mode' option with 'cors'
+        };
+
+        fetch(`${BASE_URL}${id}`, httpHeaders)
+            .then((response) => {
+                if (!response.ok) {
+                    throw new Error('Network response was not ok');
+                }
+                return response.json();
+            })
+            .then(() => {
+                load();
+            })
+            .catch((err) => {
+                console.error(err);
+            });
+
+    }
+    inputs.createTaskBtn.addEventListener('click', addTask);
+    function addTask(event) {
+        event.preventDefault();
+        const {  description } = inputs;
+        const payload = JSON.stringify({
+            description: description.value,
+            status: false,
+        });
+
+        console.log(payload);
+
+        const httpHeaders = {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: payload
+        };
+        console.log(payload);
+
+        fetch(BASE_URL, httpHeaders)
+            .then(() => {
+                clearForm();
+                load();
+            }).catch(err => {
+            console.error(err);
+        })
+
+
+
+    }
+    function clearForm() {
+        Object.values(inputs)
+        inputs.description.value = '';
+    }
+    function createElement(typeOfElement, parent, content, classes, id, attributes, useInnerHtml) {
+        const htmlElement = document.createElement(typeOfElement);
+
+        if (content && useInnerHtml) {
+            htmlElement.innerHTML = content;
+        } else {
+            if (content && typeOfElement !== 'input') {
+                htmlElement.textContent = content;
+            }
+
+            if (content && typeOfElement === 'input') {
+                htmlElement.value = content;
+            }
+        }
+
+        if (classes && classes.length > 0) {
+            htmlElement.classList.add(...classes);
+        }
+
+        if (id) {
+            htmlElement.id = id;
+        }
+
+        // { src: 'link', href: 'http' }
+        if (attributes) {
+            for (const key in attributes) {
+                htmlElement.setAttribute(key, attributes[key])
+            }
+        }
+
+        if (parent) {
+            parent.appendChild(htmlElement);
+        }
+
+        return htmlElement;
+    }
+}
+
+attachEvents();
